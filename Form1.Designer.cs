8caed2228390f0bd7667c81056e8bf31f505a765
PoleEast <a1525123@gmail.com>
2023-08-03 17:27:46 +0800

    加入專案檔案。

---
+++ Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace defate_CALIN
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnexit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.CmbCardNo = new System.Windows.Forms.ComboBox();
+            this.txtcardnum = new System.Windows.Forms.TextBox();
+            this.txtslavenum = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnStart.Location = new System.Drawing.Point(81, 61);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(147, 80);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.startbtn_Click_Click);
+            // 
+            // btnexit
+            // 
+            this.btnexit.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnexit.Location = new System.Drawing.Point(81, 172);
+            this.btnexit.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnexit.Name = "btnexit";
+            this.btnexit.Size = new System.Drawing.Size(147, 80);
+            this.btnexit.TabIndex = 2;
+            this.btnexit.Text = "Exit";
+            this.btnexit.UseVisualStyleBackColor = true;
+            this.btnexit.Click += new System.EventHandler(this.btnexit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(341, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 19);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Card num:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(341, 148);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 19);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Card No:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(341, 215);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 19);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Slave num:";
+            // 
+            // CmbCardNo
+            // 
+            this.CmbCardNo.FormattingEnabled = true;
+            this.CmbCardNo.Location = new System.Drawing.Point(444, 148);
+            this.CmbCardNo.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.CmbCardNo.Name = "CmbCardNo";
+            this.CmbCardNo.Size = new System.Drawing.Size(136, 27);
+            this.CmbCardNo.TabIndex = 6;
+            // 
+            // txtcardnum
+            // 
+            this.txtcardnum.Location = new System.Drawing.Point(444, 85);
+            this.txtcardnum.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtcardnum.Name = "txtcardnum";
+            this.txtcardnum.ReadOnly = true;
+            this.txtcardnum.Size = new System.Drawing.Size(136, 27);
+            this.txtcardnum.TabIndex = 7;
+            // 
+            // txtslavenum
+            // 
+            this.txtslavenum.Location = new System.Drawing.Point(439, 208);
+            this.txtslavenum.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtslavenum.Name = "txtslavenum";
+            this.txtslavenum.ReadOnly = true;
+            this.txtslavenum.Size = new System.Drawing.Size(141, 27);
+            this.txtslavenum.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(900, 570);
+            this.Controls.Add(this.txtslavenum);
+            this.Controls.Add(this.txtcardnum);
+            this.Controls.Add(this.CmbCardNo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnexit);
+            this.Controls.Add(this.btnStart);
+            this.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnexit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox CmbCardNo;
+        private System.Windows.Forms.TextBox txtcardnum;
+        private System.Windows.Forms.TextBox txtslavenum;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using PCI_DMC;
+using PCI_DMC_ERR;
+namespace defect_CALIN
+{
+    public partial class Form1 : Form
+    {
+        short existcard = 0, rc;
+        ushort gCardNo = 0, DeviceInfo = 0;
+        ushort[] gCardNoList = new ushort[16];
+        uint[] SlaveTable = new uint[4];
+        ushort[] NodeID = new ushort[32];
+        byte[] value = new byte[10];
+        ushort gNodeNum;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void startbtn_Click_Click(object sender, EventArgs e)
+        {
+            ushort i, card_no = 0, lMask = 0x1;
+            uint DeviceType = 0, IdentityObject = 0;
+
+            //開啟軸卡
+            rc = CPCI_DMC.CS_DMC_01_open(ref existcard);    //existcard會填入卡的數量,rc為錯誤檢測
+            if (existcard <= 0)
+                MessageBox.Show("No DMC-NET card can be found!");
+            else
+                txtcardnum.Text = existcard.ToString();
+            txtslavenum.Text = "0";
+            CmbCardNo.Items.Clear();
+
+            //初始化軸卡
+            for (i = 0; i < existcard; i++)     //進行軸卡初始化
+            {
+                rc = CPCI_DMC.CS_DMC_01_get_CardNo_seq(i, ref card_no);
+                gCardNoList[i] = card_no;
+
+                CmbCardNo.Items.Insert(i, card_no);
+
+                rc = CPCI_DMC.CS_DMC_01_pci_initial(gCardNoList[i]);
+                if (rc != 0)
+                    MessageBox.Show("Can't boot PCI_DMC Master Card!");
+
+                rc = CPCI_DMC.CS_DMC_01_initial_bus(gCardNoList[i]);
+                if (existcard != 0)
+                {
+                    CmbCardNo.SelectedIndex = 0;
+                    gCardNo = gCardNoList[0];
+                }
+            }
+
+            //建立通訊
+            gNodeNum = 0;
+            txtslavenum.Text = "0";
+            for (i = 0; i < 4; i++)
+                SlaveTable[i] = 0;
+            rc = CPCI_DMC.CS_DMC_01_start_ring(gCardNo, 0);
+            rc = CPCI_DMC.CS_DMC_01_get_device_table(gCardNo, ref DeviceInfo);
+            rc = CPCI_DMC.CS_DMC_01_get_node_table(gCardNo, ref SlaveTable[0]);
+
+            if (SlaveTable[0] == 0)
+                MessageBox.Show("CardNo: " + gCardNo.ToString() + " No slave found!");
+            else
+            {
+                for (i = 0; i < 32; i++)
+                {
+                    if ((SlaveTable[0] & lMask) != 0)
+                    {
+                        NodeID[gNodeNum] = (ushort)(i + 1);
+                        gNodeNum++;
+                        rc = CPCI_DMC.CS_DMC_01_get_devicetype(gCardNo, (ushort)(i + 1), (ushort)0, ref DeviceType, ref IdentityObject);
+                        if (rc != 0)
+                        {
+                            MessageBox.Show("get_devicetype failed - code=" + rc);
+                        }
+                        else
+                        {
+                            switch (DeviceType)
+                            {
+                                case 0x14100191:			//Remote Module 04PI
+                                    cmb04PInode.Items.Add(i + 1);
+                                    rc = CPCI_DMC.CS_DMC_01_set_rm_04pi_ipulser_mode(gCardNo, (ushort)(i + 1), (ushort)0, (ushort)1);//設定脈波介面模組輸入相位模式
+                                    p++;
+                                    break;
+                                case 0x04020192:			//DRIVER Servo A2 series
+                                    cmbDRIVERnode.Items.Add(i + 1);
+                                    p++;
+                                    break;
+                                case 0x04130191:			//Remote Module I/O
+                                    cmbIOnode.Items.Add(i + 1);
+                                    p++;
+                                    break;
+
+                            }
+                        }
+                    }
+                    lMask <<= 1;
+                }
+                if (p == 0)
+                {
+                    MessageBox.Show("No  A2  04PI DEVICE Found!");
+                }
+                else
+                {
+                    dmc04pi_init();
+                    txtSlaveNum.Text = gNodeNum.ToString();
+                    cmb04PInode.Enabled = true;
+                    cmbDRIVERnode.Enabled = true;
+                    cmbIOnode.Enabled = true;
+
+                    cmb04PInode.SelectedIndex = 0;
+                    cmbDRIVERnode.SelectedIndex = 0;
+                    cmbIOnode.SelectedIndex = 0;
+
+                    timer4PI.Enabled = true;
+                    DMCtimer.Enabled = true;
+                    /////io_active////////////////////
+                    ushort nodeid, SlotID = 0, Enable;
+                    nodeid = 9;
+                    Enable = 1;
+                    rc = CPCI_DMC.CS_DMC_01_set_rm_output_active(gCardNo, nodeid, SlotID, Enable);
+                    timerIO.Enabled = true;
+
+
+                }
+            }
+        }
+        private void btnexit_Click(object sender, EventArgs e)
+        {
+            ushort i;
+            timer1.Enabled = false;
+            for (i = 0; i < existcard; i++) rc = CPCI_DMC.CS_DMC_01_reset_card(gCardNoList[i]);
+
+            CPCI_DMC.CS_DMC_01_close();
+            Application.Exit();
+        }
+
+    }
+}
